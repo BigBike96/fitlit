c22b91e7df3275e7e79185c085012cd72e376306
Jim Charnesky <mijnesky@hotmail.com>
2021-03-30 17:37:31 -0600

    Add code to connect file to other files

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,1 +1,15 @@
-// user repo test
+// user repo test   //
+const chai = require('chai');
+const expect = chai.expect;
+
+const UserRepository = require('../src/UserRepository');
+
+describe('UserRepository', () => {
+  let userRepository;
+
+  beforeEach(() => {
+    userRepository = new UserRepository();
+  });
+
+   
+});
