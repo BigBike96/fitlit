82b841b3152d4659531674183b55648345989e28
Jim Charnesky <mijnesky@hotmail.com>
2021-03-30 20:29:55 -0600

    Add test and property for username

--- src/User.js
+++ src/User.js
@@ -1,1 +1,17 @@
 // user
+class User {
+
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+  }
+
+  returnFirstName() {
+    //returns only the first name of the name string
+    //const firstName = this.name.split("")
+    //console.log(firstName[0]);
+    //return firstName[0];
+  }
+}
+
+module.exports = User;
--- test/User-test.js
+++ test/User-test.js
@@ -1,1 +1,35 @@
-// user test
+// user test //
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/User');
+
+describe('User', () => {
+  let user;
+
+  beforeEach(() => {
+    data = {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [16, 4, 8]
+      },
+      user = new User(data);
+  });
+
+  it('should instantiate a new user', () => {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+  it('should take in a user id', () => {
+    expect(user.id).to.equal(1);
+  });
+
+  it('should have a name', () => {
+    expect(user.name).to.equal("Luisa Hane");
+  });
+
+})
