ae0c6481f6d676bbdd77aea9143bd2be32243850
Jim Charnesky <mijnesky@hotmail.com>
2021-03-30 20:51:17 -0600

    Add test and method to return a user first name only

--- src/User.js
+++ src/User.js
@@ -1,6 +1,5 @@
 // user
 class User {
-
   constructor(user) {
     this.id = user.id;
     this.name = user.name;
@@ -12,10 +11,10 @@ class User {
   }
 
   returnFirstName() {
-    //returns only the first name of the name string
-    //const firstName = this.name.split("")
-    //console.log(firstName[0]);
-    //return firstName[0];
+    // returns only the first name of the name string
+    const firstName = this.name.split(" ");
+    console.log(firstName[0]);
+    return firstName[0];
   }
 }
 
--- test/User-test.js
+++ test/User-test.js
@@ -51,4 +51,22 @@ describe('User', () => {
   it('should take in a user friends', () => {
     expect(user.friends).to.deep.equal([16, 4, 8]);
   });
+
+  it('should return a user first name', () => {
+    expect(user.returnFirstName()).to.equal("Luisa");
+
+  });
+
+
+  // it('should take in correct key values', () => {
+  //   expect(user.id).to.equal(1);
+  //   expect(user.name).to.equal("Luisa Hane");
+  //   expect(user.address).to.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
+  //   expect(user.email).to.equal("Diana.Hayes1@hotmail.com");
+  //   expect(user.strideLength).to.equal(4.3);
+  //   expect(user.dailyStepGoal).to.equal(10000);
+  //   expect(user.friends).to.deep.equal([16, 4, 8]);
+  // });
+
+
 })
