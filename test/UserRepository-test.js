d2905d214e681802cb33acd5a192220a740af016
Jim Charnesky <mijnesky@hotmail.com>
2021-03-30 18:21:06 -0600

    Add test for holding all user data

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -8,8 +8,31 @@ describe('UserRepository', () => {
   let userRepository;
 
   beforeEach(() => {
-    userRepository = new UserRepository();
+    userRepository = new UserRepository(
+      [{
+          "id": 1,
+          "name": "Luisa Hane",
+          "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+          "email": "Diana.Hayes1@hotmail.com",
+          "strideLength": 4.3,
+          "dailyStepGoal": 10000,
+          "friends": [16, 4, 8]
+        },
+        {
+          "id": 2,
+          "name": "Jarvis Considine",
+          "address": "30086 Kathryn Port, Ciceroland NE 07273",
+          "email": "Dimitri.Bechtelar11@gmail.com",
+          "strideLength": 4.5,
+          "dailyStepGoal": 5000,
+          "friends": [9, 18, 24, 19]
+        }
+      ]
+    );
   });
 
-   
+  it('should take in all user data', () => {
+    expect(userRepository.users[0]["id"]).to.equal(1);
+  });
+
 });
