9877ffc03419c8407ed121f05c536012c567fca9
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-01 19:07:04 -0400

    Pass findUsersHydrationData tests

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -1,1 +1,32 @@
 // hydration repository
+class HydrationRepository {
+  constructor(data) {
+    this.hydrationData = data;
+  }
+
+  findUsersHydrationData(id) {
+    //returns correct user hydration data
+    const usersData = this.hydrationData.filter(element => element.userID === id);
+    return usersData;
+  }
+
+
+  //
+  // calculateAllTimeOunces() {
+  //   //-iterate through all data
+  //   //using an id to take objects with matching id and
+  //   //-placing them in a new array
+  //   //-iterate through new array,
+  //   //- for each obj? add all fluid ounces of each day
+  //   //- return divided sum by the arrays.length
+  // }
+  //
+  // returnWeeksOunces() {
+  //   //!SPICY! date.js?
+  //   //-"using some week identifier"
+  //   //-create a week array of 7days
+  //   //-return ounces drank for each day
+  // }
+}
+
+module.exports = HydrationRepository;
