035ff3a2a1740532dfc9ef19ade3f1a8f8158ead
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-06 19:07:07 -0400

    Setup initial tests for Sleep Repository

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -1,1 +1,19 @@
 // sleep repos test
+const chai = require('chai');
+const expect = chai.expect;
+const sampleData = require('../test/sampleData');
+
+const SleepRepository = require('../src/SleepRepository');
+
+describe('SleepRepository', () => {
+  let sleepRepository;
+
+  beforeEach(() => {
+    sleepRepository = new SleepRepository();
+  })
+
+  it('should instantiate SleepRepository', () => {
+    expect(sleepRepository).to.be.an.instanceof(SleepRepository)
+  });
+  
+})
