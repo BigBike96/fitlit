5a59d1b805bd0016dc7cd55af3384daffc4da80d
Jim Charnesky <mijnesky@hotmail.com>
2021-04-07 16:48:31 -0600

    Clean up file

--- src/User.js
+++ src/User.js
@@ -15,6 +15,22 @@ class User {
     const firstName = this.name.split(" ");
     return firstName[0];
   }
+
+  returnFriends(userData) {
+    // const friends = this.friends.forEach((element) => {
+    //   element.filter(element.userID === id)
+    //   console.log(this.friends);
+    // });
+    console.log("THIS.FRIENDS", this.friends);
+    const friendsId = this.friends.map(element => {
+    const friendsNames = userData.find(user => user.id === element)
+
+    //return friendsNames
+  })
+  console.log("NAMES", friendsNames);
+  console.log("IDs", friendsId);
+  //return friendsId
+  }
 }
 
 
