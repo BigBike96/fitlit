d1f261868c52b2424326c3cd70eab77348641b7b
Jim Charnesky <mijnesky@hotmail.com>
2021-04-07 14:55:15 -0600

    Render sleep card to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -5,12 +5,13 @@ const userHydration = document.querySelector('#hydrationData');
 const personalStepGoal = document.querySelector('#personalStepGoal');
 const averageStepGoal = document.querySelector('#groupAverageStepGoal');
 const userSelection = document.querySelector('#userSelection');
-
+const sleepInfo = document.querySelector('#sleepData');
 
 // Global Variables
 const userRepo = new UserRepository(userData);
 const currentUser = new User(userData[getRandomIndex(userData)]);;
 const hydration = new HydrationRepository(hydrationData);
+const sleep = new SleepRepository(sleepData);
 
 // const hydateRepo = new HydrationRepository();
 
@@ -23,6 +24,8 @@ function loadUserData() {
   // helper function to invoke other functions
   displayFirstName();
   displayUserData();
+  displayHydroData();
+  displaySleepData();
 }
 
 function getRandomIndex(array) {
@@ -37,12 +40,6 @@ function displayFirstName() {
 
 
 function displayUserData() {
-  const weeksOunces = hydration.returnWeeksOunces(currentUser.id, "2019/09/22");
-  let [ day1, day2, day3, day4, day5, day6, day7 ] = weeksOunces;
-
-  console.log("TEST", day1);
-  console.log("TESTies", day1.date);
-  console.log("TESTies", day1.numOunces);
   userInfo.innerHTML += `
   <p>Address: ${currentUser.address}</p>
   <p>Email: ${currentUser.email}</p>
@@ -50,6 +47,12 @@ function displayUserData() {
   <p>Daily Step Goal: ${currentUser.dailyStepGoal}</p>
   <p>All Users Step Goal Average: ${userRepo.calculateAvgStepGoals()}</p>`
   //<p>Friends: ${currentUser.friends}</p>
+
+}
+
+function displayHydroData() {
+  const weeksOunces = hydration.returnWeeksOunces(currentUser.id, "2019/09/22");
+  let [ day1, day2, day3, day4, day5, day6, day7 ] = weeksOunces;
   userHydration.innerHTML += `
   <p>Ounces drank this day: ${hydration.returnDaysOunces(currentUser.id, "2019/09/22")}</p>
   <p>Ounces drank this week: <br>
@@ -61,4 +64,15 @@ function displayUserData() {
   ${day6.numOunces} :  ${day6.date}<br>
   ${day7.numOunces} :  ${day7.date}<br>
   </p>`
+}
+
+function displaySleepData() {
+  sleepInfo.innerHTML += `
+  <p>Latest Day Hours: </p>
+  <p>Latest Day Quality: </p>
+  <p>This Weeks Hours: </p>
+  <p>This Weeks Quality: </p>
+  <p>All Time Average Hours: </p>
+  <p>All Time Average Quality: </p>
+  `
 }
