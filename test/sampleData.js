36228a11c2dcc0350c3c961d2b10065384db1944
Jim Charnesky <mijnesky@hotmail.com>
2021-04-07 16:53:30 -0600

    Clean up file

--- test/sampleData.js
+++ test/sampleData.js
@@ -1,235 +1,205 @@
-// sample data
-// 7 for both user 1 and 2, 3 for user 3
 const sampleHydration = [{
-  "userID": 1,
-  "date": "2019/06/15",
-  "numOunces": 1
-},
-{
-  "userID": 2,
-  "date": "2019/06/15",
-  "numOunces": 2
-},
-{
-  "userID": 3,
-  "date": "2019/06/15",
-  "numOunces": 3
-},
-{
-  "userID": 1,
-  "date": "2019/06/16",
-  "numOunces": 4
-},
-{
-  "userID": 2,
-  "date": "2019/06/16",
-  "numOunces": 5
-},
-{
-  "userID": 3,
-  "date": "2019/06/16",
-  "numOunces": 6
-},
-{
-  "userID": 1,
-  "date": "2019/06/17",
-  "numOunces": 7
-},
-{
-  "userID": 2,
-  "date": "2019/06/17",
-  "numOunces": 8
-},
-{
-  "userID": 3,
-  "date": "2019/06/17",
-  "numOunces": 9
-},
-{
-  "userID": 1,
-  "date": "2019/06/18",
-  "numOunces": 10
-},
-{
-  "userID": 2,
-  "date": "2019/06/18",
-  "numOunces": 11
-},
-{
-  "userID": 1,
-  "date": "2019/06/19",
-  "numOunces": 12
-},
-{
-  "userID": 2,
-  "date": "2019/06/19",
-  "numOunces": 13
-},
-{
-  "userID": 1,
-  "date": "2019/06/20",
-  "numOunces": 14
-},
-{
-  "userID": 2,
-  "date": "2019/06/20",
-  "numOunces": 15
-},
-{
-  "userID": 1,
-  "date": "2019/06/21",
-  "numOunces": 16
-},
-{
-  "userID": 2,
-  "date": "2019/06/21",
-  "numOunces": 17
-},
-{
-  "userID": 1,
-  "date": "2019/06/22",
-  "numOunces": 18
-}
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 2
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 3
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 4
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 5
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 6
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 7
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "numOunces": 8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "numOunces": 9
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numOunces": 10
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/18",
+    "numOunces": 11
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numOunces": 12
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/19",
+    "numOunces": 13
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numOunces": 14
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/20",
+    "numOunces": 15
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numOunces": 16
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/21",
+    "numOunces": 17
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/22",
+    "numOunces": 18
+  }
 ];
 
-// SAMPLE SLEEP DATA
 const sampleSleep = [{
-  "userID": 1,
-  "date": "2019/06/15",
-  "hoursSlept": 6.1,
-  "sleepQuality": 2.2
-},
-{
-  "userID": 2,
-  "date": "2019/06/15",
-  "hoursSlept": 7,
-  "sleepQuality": 4.7
-},
-{
-  "userID": 3,
-  "date": "2019/06/15",
-  "hoursSlept": 10.8,
-  "sleepQuality": 4.7
-},
-{
-  "userID": 1,
-  "date": "2019/06/16",
-  "hoursSlept": 4.1,
-  "sleepQuality": 3.8
-},
-{
-  "userID": 2,
-  "date": "2019/06/16",
-  "hoursSlept": 7.5,
-  "sleepQuality": 3.8
-},
-{
-  "userID": 3,
-  "date": "2019/06/16",
-  "hoursSlept": 10.7,
-  "sleepQuality": 3.4
-}, {
-  "userID": 1,
-  "date": "2019/06/17",
-  "hoursSlept": 8,
-  "sleepQuality": 2.6
-},
-{
-  "userID": 2,
-  "date": "2019/06/17",
-  "hoursSlept": 5.7,
-  "sleepQuality": 3
-},
-{
-  "userID": 3,
-  "date": "2019/06/17",
-  "hoursSlept": 5.3,
-  "sleepQuality": 4.9
-},
-{
-  "userID": 1,
-  "date": "2019/06/18",
-  "hoursSlept": 10.4,
-  "sleepQuality": 3.1
-},
-{
-  "userID": 2,
-  "date": "2019/06/18",
-  "hoursSlept": 10.8,
-  "sleepQuality": 3.2
-},
-// {
-//   "userID": 3,
-//   "date": "2019/06/18",
-//   "hoursSlept": 9.8,
-//   "sleepQuality": 2.6
-// },
-{
-  "userID": 1,
-  "date": "2019/06/19",
-  "hoursSlept": 10.7,
-  "sleepQuality": 1.2
-},
-{
-  "userID": 2,
-  "date": "2019/06/19",
-  "hoursSlept": 9.6,
-  "sleepQuality": 2.5
-},
-// {
-//   "userID": 3,
-//   "date": "2019/06/19",
-//   "hoursSlept": 7.2,
-//   "sleepQuality": 3.4
-// },
-{
-  "userID": 1,
-  "date": "2019/06/20",
-  "hoursSlept": 9.3,
-  "sleepQuality": 1.2
-},
-{
-  "userID": 2,
-  "date": "2019/06/20",
-  "hoursSlept": 10.1,
-  "sleepQuality": 2.4
-},
-// {
-//   "userID": 3,
-//   "date": "2019/06/20",
-//   "hoursSlept": 9.4,
-//   "sleepQuality": 1.2
-// },
-{
-  "userID": 1,
-  "date": "2019/06/21",
-  "hoursSlept": 7.8,
-  "sleepQuality": 4.2
-},
-{
-  "userID": 2,
-  "date": "2019/06/21",
-  "hoursSlept": 4.3,
-  "sleepQuality": 4.8
-},
-{
-  "userID": 1,
-  "date": "2019/06/22",
-  "hoursSlept": 4.3,
-  "sleepQuality": 4.8
-}
-// ,
-// {
-//   "userID": 3,
-//   "date": "2019/06/21",
-//   "hoursSlept": 8.9,
-//   "sleepQuality": 3.7
-// }
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7.5,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.7,
+    "sleepQuality": 3.4
+  }, {
+    "userID": 1,
+    "date": "2019/06/17",
+    "hoursSlept": 8,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.9
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "hoursSlept": 10.4,
+    "sleepQuality": 3.1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/18",
+    "hoursSlept": 10.8,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "hoursSlept": 10.7,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/19",
+    "hoursSlept": 9.6,
+    "sleepQuality": 2.5
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "hoursSlept": 9.3,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/20",
+    "hoursSlept": 10.1,
+    "sleepQuality": 2.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "hoursSlept": 7.8,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/21",
+    "hoursSlept": 4.3,
+    "sleepQuality": 4.8
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/22",
+    "hoursSlept": 4.3,
+    "sleepQuality": 4.8
+  }
 ];
 
- //if (typeof module !== 'undefined') {
 module.exports = {
   sampleHydration,
   sampleSleep
 };
-//}
